ad64dc5d7f2184f2435c8e957c384fc4db6562f0
gilbertng007 <mkngmkng@gmail.com>
2024-10-10 10:36:26 +0800

    c

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,39 @@
+// import { ThemeProvider, CssBaseline } from '@mui/material';
+// import { createTheme } from '@mui/material/styles';
+// import { AppWrapper } from '../context/AppContext';
+// import Navbar from '../components/Navbar';
+// import Footer from '../components/Footer';
+// import '../styles/globals.css';
+// import { AnimatePresence } from 'framer-motion';
+
+// const theme = createTheme({
+//     // 自定義主題
+// });
+
+// function MyApp({ Component, pageProps, router }) {
+//     const isGamePage = router.pathname === '/game';
+
+//     return (
+//         <AppWrapper>
+//             <ThemeProvider theme={theme}>
+//                 <CssBaseline />
+//                 <div className="flex flex-col min-h-screen">
+//                     {!isGamePage && <Navbar />}
+//                     <AnimatePresence mode="wait" initial={false}>
+//                         <main className="flex-grow" key={router.pathname}>
+//                             <Component {...pageProps} />
+//                         </main>
+//                     </AnimatePresence>
+//                     {!isGamePage && <Footer />}
+//                 </div>
+//             </ThemeProvider>
+//         </AppWrapper>
+//     );
+// }
+
+// export default MyApp;
+
+
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import { createTheme } from '@mui/material/styles';
 import { AppWrapper } from '../context/AppContext';
@@ -12,13 +48,14 @@ const theme = createTheme({
 
 function MyApp({ Component, pageProps, router }) {
     const isGamePage = router.pathname === '/game';
+    const isEnglishPage = router.pathname === '/foreign-publications/english';
 
     return (
         <AppWrapper>
             <ThemeProvider theme={theme}>
                 <CssBaseline />
                 <div className="flex flex-col min-h-screen">
-                    {!isGamePage && <Navbar />}
+                    {!isGamePage && !isEnglishPage && <Navbar />}
                     <AnimatePresence mode="wait" initial={false}>
                         <main className="flex-grow" key={router.pathname}>
                             <Component {...pageProps} />
